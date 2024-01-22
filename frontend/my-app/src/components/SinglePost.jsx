d8802b315334d12796235ff902141fbe6b614273
akhilkk0803 <akhilkk0803@gmail.com>
2024-01-22 19:09:27 +0530

    Time Component

--- frontend/my-app/src/components/SinglePost.jsx
+++ frontend/my-app/src/components/SinglePost.jsx
@@ -4,38 +4,14 @@ import PostComments from "./PostComments";
 import { Avatar } from "@radix-ui/themes";
 import { motion } from "framer-motion";
 import PostImage from "./PostImage";
-import {
-  differenceInDays,
-  differenceInHours,
-  differenceInSeconds,
-  differenceInMinutes,
-} from "date-fns";
+import Time from "./util/Time";
 
 const SinglePost = ({ el }) => {
   const [comment, setComments] = useState([]);
   useEffect(() => {
     setComments(el.comments);
   }, []);
-  let time, type;
-  const getTime = (...fn) => {
-    for (let i = 0; i < fn.length; i++) {
-      const temp = fn[i](new Date(), el.createdAt);
-      if (temp > 0) {
-        time = temp;
-        console.log(fn);
-        type = fn[i].name.substring(12);
-        return true;
-      }
-    }
-    return false;
-  };
-  getTime(
-    differenceInDays,
-    differenceInHours,
-    differenceInMinutes,
-    differenceInSeconds
-  );
-  console.log(time, type);
+
   const addComment = (comment) => {
     setComments(comment);
   };
@@ -51,14 +27,12 @@ const SinglePost = ({ el }) => {
         <Avatar src={el.auth.dp} radius="full" size="3" />
         <div className="">
           <p className="font-semibold ">{el.auth.name}</p>
-          <p className="text-xs dark:text-gray-200 ">
-            <span className="font-semibold  ">{time} </span>
-            {type} ago
-          </p>
+          <Time currTime={el.createdAt} />
         </div>
       </div>
       <div>
         <p className="text-sm text-blue-400 ">{el.caption}</p>
+        <p className="text-sm text-blue-400 ">{el.hashtag}</p>
       </div>
       <PostImage el={el.image} />
       <div className="flex mt-4 mb-2 gap-10 items-center justify-center text-blue-500">
