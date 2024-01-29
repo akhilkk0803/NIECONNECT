fcccab85944be9a7bb09bce8bb35006d66a2e905
akhilkk0803 <akhilkk0803@gmail.com>
2024-01-29 17:21:01 +0530

    loading search

--- frontend/my-app/src/components/Search.jsx
+++ frontend/my-app/src/components/Search.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
 import { getUser } from "./util/users";
 import { Avatar } from "@radix-ui/themes";
-
+import { NavLink } from "react-router-dom";
 const Search = () => {
   const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
   const fetchUser = async (e) => {
+    setLoading(true);
     const res = await getUser(e.target.value, true);
+    setLoading(false);
     console.log(res);
     setSuggestions(res);
   };
   return (
     <div>
-      <input
-        type="text"
-        className="text-black w-full rounded-md"
-        placeholder="searchUser"
-        onChange={fetchUser}
-      />
+      <div className=" relative inline-block w-full">
+        <input
+          type="text"
+          className="text-black w-full  p-2  rounded-md "
+          placeholder="searchUser"
+          onChange={fetchUser}
+        />
+        {loading && (
+          <p className="absolute right-1 text-xl text-black top-0">...</p>
+        )}
+      </div>
       {suggestions.length > 0 && (
         <div className="flex flex-col gap-3">
           {suggestions.map((el) => (
-            <div className="flex">
-              <Avatar src={el.dp} />
-              <p>{el.username}</p>
-            </div>
+            <NavLink to={"/profile/" + el.username}>
+              <div className="flex">
+                <Avatar src={el.dp} />
+                <p>{el.username}</p>
+              </div>
+            </NavLink>
           ))}
         </div>
       )}
