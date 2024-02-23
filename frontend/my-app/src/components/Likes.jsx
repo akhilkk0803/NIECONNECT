8a01dae7b24f537c8fcfa825fd534aeea0b2162b
akhilkk0803 <akhilkk0803@gmail.com>
2024-02-23 23:33:28 +0530

    images

--- frontend/my-app/src/components/Likes.jsx
+++ frontend/my-app/src/components/Likes.jsx
@@ -5,39 +5,38 @@ const Likes = ({ likes, likehandler, likeState }) => {
   return (
     <div className="flex items-center gap-2  ">
       <div className="relative">
-        <AnimatePresence>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            stroke-width="2 "
-            stroke="currentColor"
-            class="w-6 h-6"
-          >
-            <motion.path
-              className="cursor-pointer"
-              onClick={likehandler}
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              initial={{
-                pathLength: 0,
-              }}
-              whileTap={{
-                pathLength: [0, 1],
-                transition: {
-                  duration: 0.7,
-                  ease: "easeInOut",
-                  times: [0, 0.5, 1],
-                },
-              }}
-              animate={{
-                pathLength: 1,
-                transition: { duration: 0.7 },
-                fill: likeState ? "rgb(86, 9, 209)" : "",
-              }}
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-            />
-          </svg>
-        </AnimatePresence>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="w-6 h-6"
+          style={{ outline: "none" }}
+        >
+          <motion.path
+            className="cursor-pointer"
+            onClick={likehandler}
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            initial={{
+              pathLength: 0,
+            }}
+            whileTap={{
+              pathLength: [0, 1],
+              transition: {
+                duration: 0.7,
+                ease: "easeInOut",
+                times: [0, 0.5, 1],
+              },
+            }}
+            animate={{
+              pathLength: 1,
+              transition: { duration: 0.7 },
+              fill: likeState ? "rgb(86, 9, 209)" : "",
+            }}
+            d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+          />
+        </svg>
         {likeState && (
           <svg
             xmlns="http://www.w3.org/2000/svg"
