37e2a29e02dd66b4108ee0387496feba52160afb
akhilkk0803 <akhilkk0803@gmail.com>
2024-03-02 00:10:09 +0530

    follow error

--- frontend/my-app/src/components/Follow.jsx
+++ frontend/my-app/src/components/Follow.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { url } from "../url";
@@ -7,13 +7,15 @@ const Follow = ({ el }) => {
   const token = localStorage.getItem("token");
 
   if (!token) return;
-  console.log(el);
   const socials = useSelector((state) => state.user?.socials);
   const user = useSelector((state) => state.user?.user?._id);
   if (!user) return;
-  const [following, setFollowing] = useState(
-    socials?.following?.filter((curr) => curr === el.user._id).length > 0
-  );
+  const [following, setFollowing] = useState(false);
+  useEffect(() => {
+    setFollowing(
+      socials?.following?.filter((curr) => curr === el.user._id).length > 0
+    );
+  }, [el]);
   console.log(following);
   const handleFollow = async () => {
     try {
