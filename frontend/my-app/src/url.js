1b69149f705081c55eed1bf3230b4ab03439d620
AKHIL K KULKARNI <97751536+akhilkk0803@users.noreply.github.com>
2024-10-22 11:48:57 +0530

    Update url.js

--- frontend/my-app/src/url.js
+++ frontend/my-app/src/url.js
@@ -1,2 +1,4 @@
 //export const url = "http://localhost:8080/";
-export const url = "https://nieconnect.vercel.app/";
+//export const url = "https://nieconnect.vercel.app/";
+export const url = "https://connect-api-qz7c.onrender.com/";
+
